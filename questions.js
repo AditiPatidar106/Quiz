6d4180da7a3c71fa4ff150565b1140589231817a
AditiPatidar106 <79257892+AditiPatidar106@users.noreply.github.com>
2021-11-12 00:37:46 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -2,132 +2,116 @@
 let questions = [
     {
     numb: 1,
-    question: "Which of the following option leads to the portability and security of Java?",
-    answer: "Bytecode is executed by the JVM",
+    question: "Which of the following developed Java?",
+    answer: "James Gosling",
     options: [
-      "Bytecode is executed by JVM",
-      "The applet makes the Java code secure and portable",
-      "Use of exception handling",
-      "Dynamic binding between objects"
+      "James Gosling",
+      "Bjarne Stroustroup",
+      "Larry Wall",
+      "Rasmus Lardorf"
     ]
   },
     {
     numb: 2,
-    question: " Which of the following is not a Java features?",
-    answer: "Use of pointers",
+    question: "Which of the following developed C language?",
+    answer: "Dennis Ritchie",
     options: [
-      "Dynamic",
-      "Architecture Neutral",
-      "Use of pointers",
-      "Object-oriented"
+      "Brendan Eich",
+      "Bjarne Stroustroup",
+      "Dennis Ritchie",
+      "Rasmus Lardorf"
     ]
   },
     {
     numb: 3,
-    question: " _____ is used to find and fix bugs in the Java programs.",
-    answer: "JDB",
+    question: "Which of the following developed Python language? ",
+    answer: "Guido van Rossum",
     options: [
-      "JVM",
-      "JRE",
-      "JDK",
-      "JDB"
+      "James Gosling",
+      "Brendan Eich",
+      "Larry Wall",
+      "Guido van Rossum"
     ]
   },
     {
     numb: 4,
-    question: "What does SQL stand for?",
-    answer: "Structured Query Language",
+    question: "Which of the following developed C++ language?",
+    answer: "Bjarne Stroustroup",
     options: [
-      "Stylish Question Language",
-      "Stylesheet Query Language",
-      "Statement Question Language",
-      "Structured Query Language"
+      "James Gosling",
+      "Bjarne Stroustroup",
+      "Larry Wall",
+      "Rasmus Lardorf"
     ]
   },
     {
     numb: 5,
-    question: "What does XML stand for?",
-    answer: "eXtensible Markup Language",
+    question: "Which of the following developed Ruby language?",
+    answer: "Yukihiro Matsumoto",
     options: [
-      "eXtensible Markup Language",
-      "eXecutable Multiple Language",
-      "eXTra Multi-Program Language",
-      "eXamine Multiple Language"
+      "Guido van Rossum",
+      "Brendan Eich",
+      "Yukihiro Matsumoto",
+      "Guido van Rossum"
     ]
   },
 
   {
     numb: 6,
-    question: "What is the return type of the hashCode() method in the Object class?",
-    answer: "INT",
+    question: "Which of the following developed PHP language?",
+    answer: "Rasmus Lardorf",
     options: [
-      "INT",
-      "VOID",
-      "LONG",
-      "OBJECT"
+      "Yukihiro Matsumoto",
+      "Bjarne Stroustroup",
+      "Guido van Rossum",
+      "Rasmus Lardorf"
     ]
   },
 
   {  numb: 7,
-    question: "Which of the following tool is used to generate API documentation in HTML format from doc comments in source code?",
-    answer: "Javadoc tool",
+    question: "Which of the following developed Lisp language?",
+    answer: "John McCarthy",
     options: [
-      "javap tool",
-      "javaw command",
-      "Javadoc tool",
-      "javah command"
+      "James Gosling",
+      "John McCarthy",
+      "Yukihiro Matsumoto",
+      "Guido van Rossum"
     ]
   },
 
   {  
     numb: 8,
-    question: " Which method of the Class.class is used to determine the name of a class represented by the class object as a String?",
-    answer: "getName()",
+    question: "Which of the following developed Perl language?",
+    answer: "Larry Wall",
     options: [
-      "getName()",
-      "intern()",
-      "tiString()",
-      "getClass()"
+      "Yukihiro Matsumoto",
+      "John McCarthy",
+      "Larry Wall",
+      "Rasmus Lardorf"
     ]
   },
 
   {  
     numb: 9,
-    question: " What do you mean by nameless objects?",
-    answer: "An object that has no reference.",
+    question: "Which of the following developed Pascal language?",
+    answer: "Niklaus Wirth",
     options: [
-      "An object that has no reference.",
-      "An object of a superclass created in the subclass.",
-      "An object without having any name but having a reference.",
-      "An object created by using the new keyword."
+      "James Gosling",
+      "John McCarthy",
+      "Niklaus Wirth",
+      "Guido van Rossum"
     ]
   },
 
   {  
     numb: 10,
-    question: " Which of the following is an immediate subclass of the Panel class?",
+    question: "Which of the following developed JavaScript language?",
     answer: "Applet Class.",
     options: [
-      "Applet Class.",
-      "Window Class.",
-      "Dialog Class.",
-      "Analog Class."
+      "James Gosling",
+      "Bjarne Stroustroup",
+      "Brendan Eich",
+      "Guido van Rossum"
     ]
   },
-
-
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
 ];
